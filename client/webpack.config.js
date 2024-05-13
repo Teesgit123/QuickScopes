dd690f74e13e118cceb37cdac4ac2b11f0f5be17
Mona Lisa <109984101+Teesgit123@users.noreply.github.com>
2024-05-13 16:52:44 -0700

    Removed some unnecessary packages on the client-side for the time being, ts-loader and @babel/plugin-transform-runtime. Added react-router-dom. Added some simple components to make sure everything could compile. Simplified the webpack config for the time being. Still need to implement eslint and much more.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,74 +1,48 @@
-const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+const dotenv = require("dotenv");
 
-module.exports = [
-  {
-    entry: "./src/index.tsx",
-    mode: "development",
-    target: "web",
-    output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: "bundle.js",
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: "./public/index.html",
-      }),
-    ],
-    resolve: {
-      extensions: [".js", ".ts", ".tsx", ".jsx", ".json"],
-    },
-    module: {
-      rules: [
-        {
-          test: /\.tsx$/,
-          use: {
-            loader: "ts-loader",
-            options: {
-              configFile: "tsconfig.json",
-            },
-          },
-
-          exclude: /node_modules/,
-        },
-        {
-          test: /\.(js|jsx)$/,
-          use: {
-            loader: "babel-loader",
-            options: {
-              presets: [
-                "@babel/preset-env",
-                "@babel/preset-react",
-                "@babel/preset-typescript",
-              ],
-              plugins: ["@babel/plugin-transform-runtime"],
-            },
-          },
-          exclude: /node_modules/,
-        },
-        {
-          test: /\.(scss|sass)$/,
-          use: [
-            "style-loader",
-            {
-              loader: "css-loader",
-              options: {
-                modules: true,
-              },
-            },
-            "sass-loader",
-          ],
-          exclude: /node_modules/,
-        },
-      ],
-    },
-    devServer: {
-      static: {
-        directory: path.join(__dirname, "public"),
-      },
-      historyApiFallback: true,
-      port: 3000,
-      open: true,
-    },
+dotenv.config();
+module.exports = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  output: {
+    filename: "bundle.js",
+    path: __dirname + "/dist",
   },
-];
+  devtool: "source-map",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(css|s[ac]ss)$/i,
+        use: [
+          // creates 'style' nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS,
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new webpack.DefinePlugin({
+      "process.env": JSON.stringify(process.env),
+    }),
+  ],
+  devServer: {
+    port: 3000,
+  },
+};
